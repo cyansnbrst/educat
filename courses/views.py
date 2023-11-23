646c04a83bc0eba375329ad1cd6e20ca97f7d460
Ekaterina <litra1122@gmail.com>
2023-11-23 11:46:34 +0300

    mixins for owner added

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Course
+
+
+# Базовый миксин для получения базового набора запросов
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset().filter(owner=self.request.user)
+        return qs
+
+
+# Базовый миксин для представлений с формами
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+# Миксин для определения модели, полей и перенаправления
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = 'course/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
